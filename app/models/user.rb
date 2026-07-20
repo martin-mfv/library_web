4e7203374fa39d6272f8bd5828ccb830b25c5624
Luong Viet Dung(Martin) <dung.lv1992@gmail.com>
2026-07-20 21:05:49 +0700

    fix: reorder validations and associations in User model for clarity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 255 }
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :library_files, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 255 }
 end
